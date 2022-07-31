526909cb53c2a04ed4950cc310d621856c405dfc
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2022-07-31 20:04:42 +0200

    Bumping to v1.0.3

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as fh:
+    requires = fh.read().splitlines()
+
 setup(name='backstabbr_api',
-		version='1.0.2',
+		version='1.0.3',
 		description='Web-scraper API and Discord Bot for the online diplomacy program Backstabbr',
-		url='https://github.com/afkhurana/backstabbr_api',
+		url='https://github.com/arlm/backstabbr_api',
 		author='Arjun Khurana',
 		author_email='afkhurana@gmail.com',
 		license='MIT',
 		packages=['backstabbr_api', 'backstabbr_bot'],
-		install_requires=["discord.py", "html5print", "requests"])
+		install_requires=requires)
